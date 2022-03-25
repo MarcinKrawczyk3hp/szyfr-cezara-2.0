98843b175bf6d4ccbc4678e87c25991b0bf6147b
3109754 <3109754@TE-GDA-D-D12C28>
2022-03-25 12:57:15 +0100

    Dodaj pliki projektów.

---
+++ szyfr cezara 2.0/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Wpisz Wielkie litery");
+
+String wpisz, wypisz;
+char[] litery = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
+'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+wpisz = Convert.ToString(Console.ReadLine());
+char[] tablica = wpisz.ToCharArray();
+char[] tablicaa = new char[tablica.Length];
+
+for (int i = 0; i < tablica.Length; i++) 
+{
+    char litera = tablica[i];
+    if (tablicaa[i] == ' ' && tablica[i] == '\n') continue;
+    int a = Array.IndexOf(litery, litera);
+    int cezar = (a + 5) % 26;
+    char SC = litery[cezar];
+    tablicaa[i] = SC;
+}
+wypisz = String.Join("", tablicaa);
+Console.WriteLine(wypisz);
